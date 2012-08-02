b7d009518f70645bce5de662789db5444238809b
Alex K <jokerku@gmail.com>
2012-08-03 01:57:36 +0600

    Initial commit. Bare rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsd::Application.config.secret_token = '84d707ef7e61f440707232b042d7445d702ed26c5eb536769d5b993defcb1433ed7d6c6246c3bf80313a93ec0fe0f7cb77d318b6f4c68ddda1855cb5468ba971'
