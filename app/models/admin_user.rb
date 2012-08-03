07aec09051f298f3cf4bf128ec900cfba1ad95d6
Alex K <jokerku@gmail.com>
2012-08-03 22:48:39 +0600

    Admin user model email validation added.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,6 +8,8 @@ class AdminUser < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
+  validates :email, :presence => true, :uniqueness => true, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
+
   def admin?
     return email =~ /\A.*?@clixlocal\.net\Z/mi ? true : false
   end
