49b349909fbbe772864a9376f52a3eabe16bcd08
Alex K <jokerku@gmail.com>
2012-08-04 00:25:15 +0600

    AdminUser model validators replaced with original.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,13 +2,10 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-
-  validates :email, :presence => true, :uniqueness => true, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => 'Must be valid email address.'}
 
   def admin?
     return email =~ /\A.*?@clixlocal\.net\Z/mi ? true : false
